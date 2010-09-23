27e428fde860f8517226de3687f871fa699cb0f2
Thomas Gideon <cmdln@thecommandline.net>
2010-09-23 17:38:32 -0400

    Added script for encoding to all formats.

--- encode.bash
+++ encode.bash
@@ -22,16 +22,56 @@ else
 	post_date=$(date +%Y/%m/%d -d $3)
 fi
 echo $date
+
+title="The Command Line ${date}"
+artist="Thomas Gideon"
+cover="${HOME}/Dropbox/Public/color_cover_art.jpg"
+album="The Command Line"
+genre="Podcast"
+url="http://thecommandline.net/${post_date}/${slug}/"
+copyright="http://creativecommons.org/licenses/by-sa/3.0/us"
+comment="Weekly ${type} cast.  Email to feedback@thecommandline.net.  Show notes and license information for this episode at ${url}."
+
+flac \
+--picture="|image/jpeg|||${cover}" \
+--tag=title="${title}" \
+--tag=artist="${artist}" \
+--tag=album="${album}" \
+--tag=year="${year}" \
+--tag=genre="${genre}" \
+--tag=composer="${artist}" \
+--tag=comment="${comment}"  \
+--tag=url="${url}"  \
+--tag=copyright="${copyright}"  \
+cmdln.net_${date}.wav
+
 faac -q 100 \
 -b 96 \
 -c 44100 \
 -o cmdln.net_${date}.m4a \
---title "The Command Line ${date}" \
---artist "Thomas Gideon" \
---album "The Command Line" \
+--title "${title}"  \
+--artist "${artist}" \
+--album "${album}" \
 --year "${year}" \
---genre "Podcast" \
---writer "Thomas Gideon" \
---comment "Weekly ${type} cast.  Email to feedback@thecommandline.net.  Show notes and license information for this episode at http://thecommandline.net/${post_date}/${slug}/" \
+--genre "${genre}" \
+--writer "${artist}" \
+--comment "${comment}"  \
 --cover-art ~/Dropbox/Public/color_cover_art.jpg \
 cmdln.net_${date}.wav
+
+oggenc \
+--comment=METADATA_BLOCK_PICTURE="$(metaflac --export-picture-to=- cmdln.net_${date}.flac| base64 -w 0)"  \
+cmdln.net_${date}.flac
+
+lame -b 112 \
+--cbr \
+--tt "${title}" \
+--ta "${artist}" \
+--tl "${album}" \
+--ty "${year}" \
+--tc "${comment}" \
+--tg "${genre}" \
+--id3v2-only \
+cmdln.net_${date}.wav \
+cmdln.net_${date}.mp3
+
