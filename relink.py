d26206200fc41bd1c689800766d9777e13310e45
Thomas Gideon <cmdln@thecommandline.net>
2010-09-29 12:36:33 -0400

    Added comments.

--- relink.py
+++ relink.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python
+#
+# relink.py - A script intended for one time use to tweak a feed to re-link its
+# enclosures to appropriate URLs at the Internet Archive.
+#
+# All project files are made available under the following, BSD-new license.
+# 
+# Copyright (c) 2010, Thomas Gideon
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Thomas Gideon nor the
+#       names of additional contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from BeautifulSoup import BeautifulStoneSoup
 import re
 import urllib2
@@ -10,7 +40,9 @@ import datetime
 
 
 
+# TODO this could be re-worked to help re-write one of the existing feeds to produce an initial flac feed
 def __repoint():
+    """ Iteratest through the feed, re-writing the enclosures. """
     logging.basicConfig(level=logging.INFO,
             format='%(message)s')
     today = datetime.date.today()
@@ -69,6 +101,7 @@ def __url_info(url, mime_type, length):
     except URLError, e:
         logging.error('Failed to connect with network.')
         return (None, None)
+
 
 if __name__ == "__main__":
     __repoint()
